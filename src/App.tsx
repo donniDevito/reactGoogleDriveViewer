b5dc1e068c6769b21cb6e2593cd6c574b6c06d8b
Maxat Nukhayev <maxatnukhayev@Maxats-MacBook-Pro.local>
2020-08-13 13:22:46 +0200

    setup version

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LandingPageComponent from './components/LandingPageComponent';
-
-function App(): JSX.Element {
-  return <LandingPageComponent />;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
 
-const authDomain = 'dev-nx4a8l6a.eu.auth0.com';
-const authClientId = '3ZXrLKI0xulQ6F2kTL8H8q47zJq1j3Cl';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './views/ApplicationView';
+
 const authRedirectUri = 'http://localhost:3000';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={authDomain}
-      clientId={authClientId}
-      redirectUri={authRedirectUri}
-    >
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
+        redirectUri={authRedirectUri}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
